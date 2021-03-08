65d1d87a382ebc5c3b2960f377e047537bee9f18
cameron-kent-sae <1020666@student.sae.edu.au>
2021-03-08 14:01:17 +1100

    ui work

---
+++ What was lost game/Assets/Cameron/UI Carpark/PhotoAlbum.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotoAlbum : MonoBehaviour
+{
+    public RectTransform parentPanel;
+    public Sprite[] photos;
+    public int imageNum;
+
+    public GameObject prevPhoto, nextPhoto;
+
+    private void Start()
+    {
+        imageNum = 0;
+
+        UpdatePhoto();
+    }
+
+    {
+        if (imageNum == 0)
+        {
+            prevPhoto.SetActive(false);
+        }
+        else
+        {
+            prevPhoto.SetActive(true);
+        }
+
+        if (imageNum == photos.Length)
+        {
+            nextPhoto.SetActive(false);
+        }
+        else
+        {
+            nextPhoto.SetActive(true);
+        }
+
+    }
+}
