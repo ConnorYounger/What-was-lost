67819723d37badaada434298ec222ded3c8dcb3e
David Strudwick <david.strudwick1301@gmail.com>
2021-03-18 09:11:27 +1100

    Prefab groups

--- What was lost game/Assets/David/CarparkGen.cs
+++ What was lost game/Assets/David/CarparkGen.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class CarparkGen : MonoBehaviour
 {   public Transform[] spawns;
     public GameObject[] prefab;
-    // Start is called before the first frame update
+    //Creating the Arrays
     void Start()
-    {
+    { // Randomlu picks the number of cars to spawn, runs the spawn script that many times
         int carNum = Random.Range(1,5);
         for(int i=0; i<carNum; ++i)
         {
@@ -15,12 +15,14 @@ public class CarparkGen : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
+   
     void Spawn()
     {
         int spotNum = Random.Range(0, 8);
         int prefabNum = Random.Range(0, 3);
 
         Instantiate(prefab[prefabNum], spawns[spotNum].position, spawns[spotNum].rotation);
+        //todo - delete the spawn number after its picked to prevent duplicates
+        //additionally - set the random range to reflect this (random.range(0, 8-i) ought to work)
     }
 }
