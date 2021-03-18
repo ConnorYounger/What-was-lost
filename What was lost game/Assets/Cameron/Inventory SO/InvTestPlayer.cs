67b8baa8b0172e06d30428c3f32385d58f5bbb75
cameron-kent-sae <1020666@student.sae.edu.au>
2021-03-18 13:23:40 +1100

    Implemented test inventory
    add items on collision to test inventory

---
+++ What was lost game/Assets/Cameron/Inventory SO/InvTestPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvTestPlayer : MonoBehaviour
+{
+    public InventoryObject inventory;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var item = other.GetComponent<Item>();
+        if(item)
+        {
+            inventory.AddItem(item.item);
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        inventory.Container.Clear();
+    }
+}
