78aeaeda3f82ecc71e9a5021ad0bacaffb3bdf2f
cameron-kent-sae <1020666@student.sae.edu.au>
2021-03-18 15:56:16 +1100

    inventory display work

---
+++ What was lost game/Assets/Cameron/Inventory SO/DisplayInventory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayInventory : MonoBehaviour
+{
+    public GameObject inventoryPage, inventorySlot;
+    public InventoryObject inventory;
+    public Dictionary<InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventorySlot, GameObject>();
+
+    private void Start()
+    {
+        inventoryPage = GameObject.Find("Page Inventory");
+
+        CreateDisplay();
+    }
+
+    private void Update()
+    {
+        UpdateDisplay();
+    }
+
+    private void CreateDisplay()
+    {
+        for (int i = 0; i < inventory.Container.Count; i++)
+        {
+            var newSlot = Instantiate(inventorySlot);
+            newSlot.transform.SetParent(inventoryPage.transform);
+        }
+    }
+
+   
+    private void UpdateDisplay()
+    {
+    }
+}
