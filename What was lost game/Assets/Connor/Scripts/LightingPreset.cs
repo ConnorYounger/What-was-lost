4f8ab4029a7e23b5246c4bc4d3d61064760158a4
ConnorYounger <72112139+ConnorYounger@users.noreply.github.com>
2021-03-21 21:45:09 +1100

    Time Manager (Day Cycle v1)

---
+++ What was lost game/Assets/Connor/Scripts/LightingPreset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Lighting Preset", menuName = "Lighting/Lighting Preset")]
+
+public class LightingPreset : ScriptableObject
+{
+    public Gradient AmbientColor;
+    public Gradient DirectionalColor;
+    public Gradient FogColor;
+    public Gradient skyBoxColor;
+}
